6a986ce532214f0e2d806dbbe5517d35f942c1ef
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-20 16:20:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,12 +9,12 @@ def array_with_two_elements
 end
 
 def first_element (my_first_element)
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = @taylor_swift[0]
 end
 
 def third_element (my_third_element)
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_third_element = @taylor_swift[2]
 end
 
