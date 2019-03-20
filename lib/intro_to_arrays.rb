85bbb4365876d97c0c7f3f0b25996df368f22f4c
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-20 16:18:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,6 +8,8 @@ def array_with_two_elements
   @my_two_array = array_with_two_elements
 end
 
+def first_element 
+
 def third_element (my_third_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
   my_third_element = @taylor_swift[2]
